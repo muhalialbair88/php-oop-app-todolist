2799ee357a50ec91e424eb6fea671efc14300ed5
muhalialbair88 <muhalialbair88@gmail.com>
2021-12-30 10:05:42 +0700

    initial-commit

---
+++ Test/TodolistServiceTest.php
@@ -0,0 +1,63 @@
+<?php
+
+use Service\TodolistServiceImpl;
+
+require_once __DIR__ . "/../Entity/Todolist.php";
+require_once __DIR__ . "/../Repository/TodolistRepository.php";
+require_once __DIR__ . "/../Service/TodolistService.php";
+
+use Entity\Todolist;
+use Service\TodolistService;
+use Repository\TodolistRepositoryImpl;
+
+function testShowTodolist(): void
+{
+    $todolistRepository = new TodolistRepositoryImpl();
+    $todolistRepository->todolist[1] = new Todolist("Belajar PHP");
+    $todolistRepository->todolist[2] = new Todolist("Belajar PHP Dasar");
+    $todolistRepository->todolist[3] = new Todolist("Belajar PHP OOP");
+
+    $todolistService = new TodolistServiceImpl($todolistRepository);
+
+    $todolistService->showTodolist();
+}
+
+function testAddTodolist(): void
+{
+    $todolistRepository = new TodolistRepositoryImpl();
+
+    $todolistService = new TodolistServiceImpl($todolistRepository);
+    $todolistService->addTodolist("Belajar PHP");
+    $todolistService->addTodolist("Belajar PHP Dasar");
+    $todolistService->addTodolist("Belajar PHP OOP");
+
+    $todolistService->showTodolist();
+}
+
+function testRemoveTodolist(): void
+{
+    $todolistRepository = new TodolistRepositoryImpl();
+
+    $todolistService = new TodolistServiceImpl($todolistRepository);
+    $todolistService->addTodolist("Belajar PHP");
+    $todolistService->addTodolist("Belajar PHP Dasar");
+    $todolistService->addTodolist("Belajar PHP OOP");
+
+    $todolistService->showTodolist();
+
+    $todolistService->removeTodolist(1);
+    $todolistService->showTodolist();
+
+    $todolistService->removeTodolist(4);
+    $todolistService->showTodolist();
+
+    $todolistService->removeTodolist(1);
+    $todolistService->showTodolist();
+
+    $todolistService->removeTodolist(1);
+    $todolistService->showTodolist();
+}
+
+// testShowTodolist();
+// testAddTodolist();
+testRemoveTodolist();
